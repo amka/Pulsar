c5e34dd788e008b15f781f2a2e4c947de7b7e763
Andrey M <meamka@ya.ru>
2013-06-02 14:44:57 +0400

    Initial Commit

---
+++ Pulsar/MEDocument.h
@@ -0,0 +1,13 @@
+//
+//  MEDocument.h
+//  Pulsar
+//
+//  Created by Andrey M. on 02.06.13.
+//  Copyright (c) 2013 Andrey M. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MEDocument : NSDocument
+
+@end
---
+++ Pulsar/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Pulsar
+//
+//  Created by Andrey M. on 02.06.13.
+//  Copyright (c) 2013 Andrey M. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
